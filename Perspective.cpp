a003404983613c8fbaafa2f88c798fc0309d3ace
lcjury <zeui16@gmail.com>
2015-03-13 01:06:49 -0600

    mensaje

--- Perspective.cpp
+++ Perspective.cpp
@@ -6,37 +6,12 @@
 #include <cmath>
 #include <string>
 
-
 static void onMouse(  int event, int x, int y, int , void*)
 {
 	if( event != CV_EVENT_LBUTTONDOWN )
 		return;
 
 }
-
-IplImage * AffineImage( IplImage * src )
-{
-	IplImage * res = cvCreateImage( cvSize(src->width, src->height), src->depth, src->nChannels );
-	cvSet( res, CV_RGB(0,0,0) );
-
-	CvPoint2D32f  srcTriang[3];
-	srcTriang[0] = cvPoint2D32f(0,0);
-	srcTriang[1] = cvPoint2D32f( src->width,0);
-	srcTriang[2] = cvPoint2D32f(0,src->height);
-
-	CvPoint2D32f  dstTriang[3];
-	dstTriang[0] = cvPoint2D32f(50,50);
-	dstTriang[1] = cvPoint2D32f(src->width,0);
-	dstTriang[2] = cvPoint2D32f(0,src->height);
-
-
-	CvMat * M = cvCreateMat(2,3,CV_32F);
-	cvGetAffineTransform( srcTriang, dstTriang, M);
-	cvWarpAffine(src, res, M);
-	cvReleaseMat(&M);	
-	return res;
-}
-
 void drawPoly( IplImage * src, int dx1, int dy1, int dx2, int dy2, int dx3, int dy3, int dx4, int dy4 )
 {
 	CvPoint curve1[4];
@@ -55,19 +30,38 @@ void drawPoly( IplImage * src, int dx1, int dy1, int dx2, int dy2, int dx3, int 
 	cvPolyLine( src, curves, vertexCount, 1, true, cvScalar(255,255,255), 5);
 }
 
+IplImage * PerspectiveImage( IplImage * src )
+{
+	IplImage * res = cvCreateImage( cvSize(src->width, src->height), src->depth, src->nChannels );
+	cvSet( res, CV_RGB(0,0,0) );
+
+	CvPoint2D32f  srcTriang[4];
+	srcTriang[0] = cvPoint2D32f(0,0);
+	srcTriang[1] = cvPoint2D32f(src->width,0);
+	srcTriang[2] = cvPoint2D32f(0,src->height);
+	srcTriang[3] = cvPoint2D32f(src->width, src->height);
+
+	CvPoint2D32f  dstTriang[4];
+	dstTriang[0] = cvPoint2D32f(0,0);
+	dstTriang[1] = cvPoint2D32f(src->width,0);
+	dstTriang[2] = cvPoint2D32f(100,src->height-50);
+	dstTriang[3] = cvPoint2D32f(src->width-100,src->height-50);
+
+	CvMat * M = cvCreateMat(3,3,CV_32F);
+	cvGetPerspectiveTransform( srcTriang, dstTriang, M);
+	cvWarpPerspective(src, res, M);
+	cvReleaseMat(&M);
+	return res;
+}
 int main(int argc, const char* argv[])
 {
 	IplImage * src = cvLoadImage("lena.jpg", 1);
 	cvNamedWindow("Lena", CV_WINDOW_AUTOSIZE);
-	cvNamedWindow("Lena2", CV_WINDOW_AUTOSIZE);
 	drawPoly(src,0,0,src->width,0,src->width, src->height,0,src->height);
 
-	cvSetMouseCallback( "Lena", onMouse );
+	cvShowImage("Original", src);
+	cvShowImage("Traslade", PerspectiveImage(src));
 
-	cvShowImage("Lena", src);
-	cvShowImage("Lena2", AffineImage(src));
-	int dist;
-	//cvCreateTrackbar("Q: ", "Lena2", &dist, src->
 
 	cvWaitKey();
 	return 0;
